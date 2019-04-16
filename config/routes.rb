d9540311e06a84f700b336c614f9103f0658d1bc
github username <github email address>
2019-04-16 19:54:52 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  # get 'authors/:id/posts', to: 'authors#posts_index'
+  # get 'authors/:id/posts/:post_id', to: 'authors#post'
+
+  resources :authors, only: [:show] do
+   # nested resource for posts
+   resources :posts, only: [:show, :index]
+ end
+
   root 'posts#index'
 end
