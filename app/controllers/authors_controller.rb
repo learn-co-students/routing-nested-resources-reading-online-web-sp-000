5892f9ae5b749ab90e6749d652bd1d495017830b
Ramon Chavez <rachavez1974@gmail.com>
2019-08-25 17:25:49 -0400

    Finish reading

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,14 @@
 class AuthorsController < ApplicationController
-
   def show
     @author = Author.find(params[:id])
+  end
+
+  def index
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
   end
 
 end
