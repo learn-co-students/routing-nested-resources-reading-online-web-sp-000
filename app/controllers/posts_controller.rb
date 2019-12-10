2f48b62ae077c936e36b56d7392bd6df07275931
Bernadette Davis <bsd231@nyu.edu>
2019-12-10 01:17:19 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    if params[:author_id] #filters posts by author
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all #just retrieves all posts
+    end
   end
 
   def show
