81926e4299afc3dd0284bc64356eab7b6baa5f4f
CoderJay06 <jaycru06@yahoo.com>
2020-07-01 16:34:07 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,12 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  # all posts nested under an author:
+  # author_post_path(author_id, post_id)
+
 end
+
+
+
+
+
