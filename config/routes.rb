246dd5cd68d22402bdb38dfd4c2828666f240d1b
Oscar Shoudt <shoudto@gmail.com>
2020-09-23 21:43:08 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  
+  # get 'authors/:id/posts', to: 'authors#posts_index'    # we need explicitly tell them where
+  # get 'authors/:id/posts/:post_id', to: 'authors#posts' # to route to it causes an error.
+  # Now I am recreating the routes as nested resources
+  resources :authors, only: [:show] do 
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
-  get 'authors/:id/posts', to: 'authors#posts_index'    # we need explicitly tell them where to route to
-  get 'authors/:id/posts/:post_id', to: 'authors#posts' # it causes an error.
+
 end
