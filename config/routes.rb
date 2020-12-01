fef3d7e03ffca12b4644c2cd6c35ed7cce9b75c0
sparsell <hello@sparsell.com>
2020-12-01 06:27:58 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  resources :authors, only: [:show, :index] do 
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+    resources :posts, only: [:show, :index]
+  end
+
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
 
