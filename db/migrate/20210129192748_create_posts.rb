9791d36ded827bbfe4c6d8614e613ed4b25c00c1
nllew <n.lewis.perc@gmail.com>
2021-01-29 14:00:39 -0600

    Done.

--- db/migrate/20210129192748_create_posts.rb
+++ db/migrate/20210129192748_create_posts.rb
@@ -1,10 +1,8 @@
-class CreatePosts < ActiveRecord::Migration
+class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.string :title
       t.text :description
-
-      t.timestamps null: false
     end
   end
 end
--- db/migrate/20210129192944_add_post_status_to_posts.rb
+++ db/migrate/20210129192944_add_post_status_to_posts.rb
@@ -1,4 +1,4 @@
-class AddPostStatusToPosts < ActiveRecord::Migration
+class AddPostStatusToPosts < ActiveRecord::Migration[5.0]
   def change
     add_column :posts, :post_status, :boolean
   end
