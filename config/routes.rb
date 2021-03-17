c36fcf7ba32adc78232707ae6ca16d19bfaa31a0
a-laz <mortaleusmarketing@gmail.com>
2021-03-17 16:16:18 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
-
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
+    # get 'authors/:id/posts', to: 'authors#post_index'
+    # get 'authors/:id/posts/:post_id', to: 'authors#post'
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
